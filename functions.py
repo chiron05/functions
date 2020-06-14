f73c072d8886be4ac7333b9cc1e0f5bad243216b
chiron05 <66912379+chiron05@users.noreply.github.com>
2020-06-14 19:29:05 +0400

    functions.py
    It will print out the numbers of characters present in  String

---
+++ functions.py
@@ -0,0 +1,110 @@
+def most_frequent():
+    string=str(input("Please enter a string: "))
+    a=string.count("a")
+    if(a>0):
+        print("a= 0" + str(a))
+
+
+    b = string.count("b")
+    if (b > 0):
+        print("b= 0" + str(b))
+
+    c = string.count("c")
+    if (c > 0):
+        print("c= 0" + str(c))
+
+    d = string.count("d")
+    if (d > 0):
+        print("d= 0" + str(d))
+
+    e = string.count("e")
+    if (e > 0):
+        print("e= 0" + str(e))
+
+    f = string.count("f")
+    if (f > 0):
+        print("f= 0" + str(f))
+
+    g = string.count("g")
+    if (g > 0):
+        print("g= 0" + str(g))
+
+    h = string.count("h")
+    if (h > 0):
+        print("h= 0" + str(h))
+
+    i = string.count("i")
+    if (i > 0):
+        print("i= 0" + str(i))
+
+    j = string.count("j")
+    if (j > 0):
+        print("j= 0" + str(j))
+
+    k = string.count("k")
+    if (k > 0):
+        print("k= 0" + str(k))
+
+    l = string.count("l")
+    if (l > 0):
+        print("l= 0" + str(l))
+
+    m = string.count("m")
+    if (m > 0):
+        print("m= 0" + str(m))
+
+    n = string.count("n")
+    if (n > 0):
+        print("n= 0" + str(n))
+
+    o = string.count("o")
+    if (o > 0):
+        print("o= 0" + str(o))
+
+    p = string.count("p")
+    if (p > 0):
+        print("p= 0" + str(p))
+
+    q = string.count("q")
+    if (q > 0):
+        print("q= 0" + str(q))
+
+    r = string.count("r")
+    if (r > 0):
+        print("r= 0" + str(r))
+
+    s = string.count("s")
+    if (s > 0):
+        print("s= 0" + str(s))
+
+    t = string.count("t")
+    if (t > 0):
+        print("t= 0" + str(t))
+
+    u = string.count("u")
+    if (u > 0):
+        print("u= 0" + str(u))
+
+    v = string.count("v")
+    if (v > 0):
+        print("v= 0" + str(v))
+
+    w = string.count("w")
+    if (w > 0):
+        print("w= 0" + str(w))
+
+    x = string.count("x")
+    if (x > 0):
+        print("x= 0" + str(x))
+
+    y = string.count("y")
+    if (y > 0):
+        print("y= 0" + str(y))
+
+    z = string.count("z")
+    if (z > 0):
+        print("z= 0" + str(z))
+
+
+
+most_frequent()
